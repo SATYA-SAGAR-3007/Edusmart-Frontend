bba6c95d50425b3f66a718b438bf395238534648
SATYA-SAGAR-3007 <sssagarrao3007@gmail.com>
2026-03-29 19:40:50 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -1,61 +1,57 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./layout/Layout"
+import Layout from "./layout/Layout";
 
-import Login from "./pages/Login"
-import TeacherLogin from "./pages/TeacherLogin"
-import StudentLogin from "./pages/StudentLogin"
+import Login from "./pages/Login";
+import TeacherLogin from "./pages/TeacherLogin";
+import StudentLogin from "./pages/StudentLogin";
 
-import TeacherSignup from "./pages/TeacherSignup"
-import StudentSignup from "./pages/StudentSignup"
+import TeacherSignup from "./pages/TeacherSignup";
+import StudentSignup from "./pages/StudentSignup";
 
-import Dashboard from "./pages/Dashboard"
-import Students from "./pages/Students"
-import Attendance from "./pages/Attendance"
-import Assignments from "./pages/Assignments"
-import Performance from "./pages/Performance"
-import AIInsights from "./pages/AIInsights"
-import RiskDashboard from "./pages/RiskDashboard"
+import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
+import Attendance from "./pages/Attendance";
+import Assignments from "./pages/Assignments";
+import Performance from "./pages/Performance";
+import AIInsights from "./pages/AIInsights";
+import RiskDashboard from "./pages/RiskDashboard";
 
-import StudentDashboard from "./pages/StudentDashboard"
+import StudentDashboard from "./pages/StudentDashboard";
 import StudentPerformance from "./pages/StudentPerformance";
 import StudentTasks from "./pages/StudentTasks";
 import StudentInsights from "./pages/StudentInsights";
 
-function App(){
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
 
-return(
+        {/* Public pages */}
+        <Route path="/" element={<Login />} />
+        <Route path="/teacher-signup" element={<TeacherSignup />} />
+        <Route path="/teacher-login" element={<TeacherLogin />} />
+        <Route path="/student-login" element={<StudentLogin />} />
 
-<BrowserRouter>
+        {/* Student pages */}
+        <Route path="/student-dashboard/:id" element={<StudentDashboard />} />
+        <Route path="/student-performance/:id" element={<StudentPerformance />} />
+        <Route path="/student-tasks/:id" element={<StudentTasks />} />
+        <Route path="/student-insights/:id" element={<StudentInsights />} />
 
-<Routes>
+        {/* Teacher pages with sidebar */}
+        <Route path="/student-signup" element={<StudentSignup />} />
+        <Route path="/teacher-dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/students" element={<Layout><Students /></Layout>} />
+        <Route path="/attendance" element={<Layout><Attendance /></Layout>} />
+        <Route path="/assignments" element={<Layout><Assignments /></Layout>} />
+        <Route path="/performance" element={<Layout><Performance /></Layout>} />
+        <Route path="/insights" element={<Layout><AIInsights /></Layout>} />
+        <Route path="/risk" element={<Layout><RiskDashboard /></Layout>} />
 
-{/* Public pages */}
-
-<Route path="/" element={<Login/>}/>
-<Route path="/teacher-signup" element={<TeacherSignup/>}/>
-<Route path="/teacher-login" element={<TeacherLogin/>}/>
-<Route path="/student-login" element={<StudentLogin/>}/>
-<Route path="/student-dashboard/:id" element={<StudentDashboard/>}/>
-<Route path="/student-performance/:id" element={<StudentPerformance />} />
-<Route path="/student-tasks/:id" element={<StudentTasks />} />
-<Route path="/student-insights/:id" element={<StudentInsights />} />
-
-{/* Teacher pages with sidebar */}
-<Route path="/student-signup" element={<StudentSignup/>}/>
-<Route path="/teacher-dashboard" element={<Layout><Dashboard/></Layout>} />
-<Route path="/students" element={<Layout><Students/></Layout>} />
-<Route path="/attendance" element={<Layout><Attendance/></Layout>} />
-<Route path="/assignments" element={<Layout><Assignments/></Layout>} />
-<Route path="/performance" element={<Layout><Performance/></Layout>} />
-<Route path="/insights" element={<Layout><AIInsights/></Layout>} />
-<Route path="/risk" element={<Layout><RiskDashboard/></Layout>} />
-
-</Routes>
-
-</BrowserRouter>
-
-)
-
+      </Routes>
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
